304ac1f2b95334b61ad64f76a64c0ff34a146e9b
Arkady Chelyadnikov <prbrq@ya.ru>
2023-02-02 21:31:12 +0300

    Cleanup

--- ImageTextOCR/DownloadedLanguages.cs
+++ ImageTextOCR/DownloadedLanguages.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TesseractOCR.Enums;
 
 namespace ImageTextOCR
@@ -12,8 +9,8 @@ namespace ImageTextOCR
     {
         public static List<Language> Languages { get; set; } = new List<Language>();
 
-        private static readonly Dictionary<string, Language> languageFiles = new Dictionary<string, Language>() 
-        { 
+        private static readonly Dictionary<string, Language> languageFiles = new Dictionary<string, Language>()
+        {
             { "eng.traineddata", Language.English },
             { "rus.traineddata", Language.Russian },
             { "pol.traineddata", Language.Polish },
--- ImageTextOCR/ImageInfo.cs
+++ ImageTextOCR/ImageInfo.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using TesseractOCR;
+﻿using TesseractOCR;
 using TesseractOCR.Enums;
 
 namespace ImageTextOCR
